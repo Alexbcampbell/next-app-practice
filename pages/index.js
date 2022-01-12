3733886f5f5557d4ff4fc17dbcaf6085e96893ff
Alex Campbell <alexbe.campbell@gmail.com>
2022-01-12 16:50:13 -0600

    created article list item component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
 
 export default function Home({articles}) {
-  console.log(articles)
+  
   return (
     <div >
       <Head><title>PRODJEX SUPPORT DASHBOARD</title>
       <meta name='keywords' content='support, web dev'/>
       </Head>
-      <h1>WELCOME TO NEXT</h1>
+      <ArticleList />
     </div>
   )
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https.jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
   const articles = await res.json()
 
   return {

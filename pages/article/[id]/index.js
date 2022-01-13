548e4ab15354c6aa932bf796122f9f91d92f3e36
Alex Campbell <alexbe.campbell@gmail.com>
2022-01-13 16:02:05 -0600

    using get static paths to get article path

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -25,4 +25,22 @@ return {
 }
 }
 
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+    const articles = await res.json()
+
+    const ids = articles.map(article => article.id)
+
+    const paths = ids.map(id => ({params: {id: id.toString()}})
+
+    return  {
+        paths, 
+        fallback: false
+        }
+    }
+
+
+
+
 export default article

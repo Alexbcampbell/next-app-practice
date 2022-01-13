964aa538b9d527102e7f0ee917a8c50bf2742ca4
Alex Campbell <alexbe.campbell@gmail.com>
2022-01-13 15:42:42 -0600

    added index with serverside props to pass article ID

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,22 @@
-const article = () => {
-    return <div>This is an Article</div>
+import { useRouter } from "next/router"
+
+const article = ({article}) => {
+    // const router = useRouter()
+    // const {id} = router.query
+
+    return <div>This is Article {article.id}</div>
+}
+
+export const getServerSideProps = async (context) => {
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+
+const article = await res.json()
+
+return {
+    props: {
+        article
+    }
+}
 }
 
 export default article

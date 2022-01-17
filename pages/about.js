39533d16c657e8638bd100c411c6d7fce9943a28
Alex Campbell <alexbe.campbell@gmail.com>
2022-01-17 16:32:43 -0600

    replace head with Meta component and pass props for article titles

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,9 @@
-import Head from "next/head"
 
 const about = () => {
     return(
         <div>
-            <Head>
                 <title>About</title>
                 <meta name='keywords' content='support, web dev'/>
-            </Head>
 
             <h1>About Page</h1>
         </div>

25e78c5ba50b7270d6b3b8da8bbcb9ef9ebfc0a6
Alex Campbell <alexbe.campbell@gmail.com>
2022-01-17 16:21:52 -0600

    change res to server from config file. Shows article data via api

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,7 +1,7 @@
 import { server } from '../../../config'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import Meta from '../../../components/Meta'
+// import Meta from '../../../components/Meta'
 
 const article = ({ article }) => {
   // const router = useRouter()
@@ -9,7 +9,7 @@ const article = ({ article }) => {
 
   return (
     <div>
-      <Meta title={article.title} description={article.excerpt} />
+      {/* <Meta title={article.title} description={article.excerpt} /> */}
       <h1>{article.title}</h1>
       <p>{article.body}</p>
       <br />
@@ -19,30 +19,56 @@ const article = ({ article }) => {
 }
 
 export const getStaticProps = async (context) => {
-  const res = await fetch(`${server}/api/articles/${context.params.id}`)
-
-  const article = await res.json()
-
-  return {
-    props: {
-      article,
-    },
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
+  
+    const article = await res.json()
+  
+    return {
+      props: {
+        article,
+      },
+    }
   }
-}
-
-export const getStaticPaths = async () => {
-  const res = await fetch(`${server}/api/articles`)
-
-  const articles = await res.json()
-
-  const ids = articles.map((article) => article.id)
-  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
-
-  return {
-    paths,
-    fallback: false,
+  
+  export const getStaticPaths = async () => {
+    const res = await fetch(`${server}/api/articles`)
+  
+    const articles = await res.json()
+  
+    const ids = articles.map((article) => article.id)
+    const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+  
+    return {
+      paths,
+      fallback: false,
+    }
   }
-}
+
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(`${server}/api/articles/${context.params.id}`)
+
+//   const article = await res.json()
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   }
+// }
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`${server}/api/articles`)
+
+//   const articles = await res.json()
+
+//   const ids = articles.map((article) => article.id)
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+//   return {
+//     paths,
+//     fallback: false,
+//   }
+// }
 
 
 export default article

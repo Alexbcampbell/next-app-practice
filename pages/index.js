c080c8f22e43489350b07e8199e3ccdf2bf235fa
Alex Campbell <alexbe.campbell@gmail.com>
2022-01-17 14:25:30 -0600

    add config folder and import server url from config index file

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
-/* eslint-disable react/jsx-key */
-import Head from 'next/head'
+
+import { server } from '../config'
 import ArticleList from '../components/ArticleList'
 
 
 export default function Home({articles}) {
   return (
     <div >
-      <Head><title>PRODJEX SUPPORT DASHBOARD</title>
-      <meta name='keywords' content='support, web dev'/>
-      </Head>
       <ArticleList articles={articles}/>
     </div>
   )
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
 
   return {
@@ -24,3 +21,14 @@ export const getStaticProps = async () => {
     },
   }
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }

19c47f577117aad4a7c65000b13f9c74a2bcd9b6
Alex Campbell <alexbe.campbell@gmail.com>
2022-01-17 14:14:39 -0600

    installed jshint, created id js for articles api

--- pages/api/articles/index.js
+++ pages/api/articles/index.js
@@ -1,13 +1,5 @@
-import { articles } from "../../../data";
+import { articles } from '../../../data'
 
-export default function handler({query:{ id }}
-    , res) {
-    
-    const filtered = articles.filter(article => article.id === id)
-
-    if(filtered.length > 0) {
-        res.status(200).json(filtered[0])
-    } else {
-        res.status(404).json({message: `Article with the ID of ${id} is not found`})
-    }
+export default function handler(req, res) {
+  res.status(200).json(articles)
 }
--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,45 +1,48 @@
-import Link from "next/link"
-import { useRouter } from "next/router"
+import { server } from '../../../config'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import Meta from '../../../components/Meta'
 
-const article = ({article}) => {
-    // const router = useRouter()
-    // const {id} = router.query
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
 
-    return <>
-    <h1>{article.title}</h1>
-    <p>{article.body}</p>
-    <br />
-    <Link href='/'>Go Back</Link>
-    </>
+  return (
+    <div>
+      <Meta title={article.title} description={article.excerpt} />
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go Back</Link>
+    </div>
+  )
 }
 
-export const getServerSideProps = async (context) => {
-const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+export const getStaticProps = async (context) => {
+  const res = await fetch(`${server}/api/articles/${context.params.id}`)
 
-const article = await res.json()
+  const article = await res.json()
 
-return {
+  return {
     props: {
-        article
-    }
-}
+      article,
+    },
+  }
 }
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const res = await fetch(`${server}/api/articles`)
 
-    const articles = await res.json()
+  const articles = await res.json()
 
-    const ids = articles.map(article => article.id)
+  const ids = articles.map((article) => article.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
 
-    // const paths = ids.map(id => ({params: {id: id.toString()}})
-
-    return {
-        paths,
-        fallback: false,
-        }
+  return {
+    paths,
+    fallback: false,
+  }
 }
-
 
 
 export default article
